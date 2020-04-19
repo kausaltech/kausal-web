634c695e9a0a498d3252a9918009a26064fec6da
Tero Tikkanen <tero.tikkanen@gmail.com>
2020-04-19 18:09:40 -0400

    Style announcement and hero. Add scroll-to-top.

--- kausal_web/static/js/kausal_web.js
+++ kausal_web/static/js/kausal_web.js
@@ -1,37 +1,54 @@
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+$(document).ready(function(){
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
       }
-    }
-  });
+    });
   
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 50) {
+               $('#back-to-top').fadeIn();
+           } else {
+               $('#back-to-top').fadeOut();
+           }
+       });
+       // scroll body to 0px on click
+       $('#back-to-top').click(function () {
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+
+});
--- landing/models.py
+++ landing/models.py
@@ -9,30 +9,96 @@ from wagtail.images.blocks import ImageChooserBlock
 
 class LandingPage(TranslatablePage):
     body = StreamField([
+        ## Hero Section
         ('hero', blocks.StructBlock([
-            ('in_navigation', blocks.CharBlock(required=False, help_text="Fill if you want this section added in page navigation",)),
+            ('in_navigation',
+                blocks.CharBlock(
+                required=False,
+                help_text="Fill if you want this section added in page navigation",)
+            ),
+            ('lead', blocks.CharBlock(required=False)),
             ('headline', blocks.CharBlock()),
+            ('content', blocks.RichTextBlock(required=False)),
+            ('cta_link', blocks.CharBlock(required=False, help_text="Link (# or url) to CTA content")),
+            ('cta_button', blocks.CharBlock(required=False, help_text="CTA Button text")),
+            ('hero_image', ImageChooserBlock(required=False)),
+        ], icon='home', template='landing/blocks/hero.html',
+        help_text="Front page main hero section",)),
+        ## Basic Content Section
+        ('content_section', blocks.StructBlock([
+            ('in_navigation', blocks.CharBlock(
+                required=False,
+                help_text="Fill if you want this section added in page navigation",)
+            ),
+            ('headline', blocks.CharBlock(required=False)),
             ('lead', blocks.CharBlock(required=False)),
             ('content', blocks.RichTextBlock()),
-            ('hero_image', ImageChooserBlock(required=False)),
-        ], icon='image', template='landing/blocks/hero.html')),
-        ('content_section', blocks.StructBlock([
-            ('in_navigation', blocks.CharBlock(required=False, help_text="Fill if you want this section added in page navigation",)),
-            ('content', blocks.RichTextBlock()),
             ('section_image', ImageChooserBlock(required=False))
-        ], icon='doc-full-inverse', template='landing/blocks/content_section.html')),
+        ], icon='doc-full-inverse', template='landing/blocks/content_section.html',
+        help_text="Basic content section")),
+        ## Product Features Section
         ('features_list', blocks.StructBlock([
-            ('in_navigation', blocks.CharBlock(required=False, help_text="Fill if you want this section added in page navigation",)),
+            ('in_navigation', blocks.CharBlock(
+                required=False,
+                help_text="Fill if you want this section added in page navigation",)
+            ),
             ('headline', blocks.CharBlock()),
+            ('lead', blocks.CharBlock(required=False)),
+            ('content', blocks.RichTextBlock(required=False)),
             ('features', blocks.ListBlock(
                 blocks.StructBlock([
                     ('title', blocks.CharBlock()),
                     ('image', ImageChooserBlock()),
                     ('description', blocks.RichTextBlock()),
                 ],
-                icon='doc-full-inverse')
-            , icon='table', template='landing/blocks/features.html')),
-        ], template='landing/blocks/features_list.html')),
+                icon='radio-full')
+            , template='landing/blocks/features.html')),
+        ], icon='list-ul', template='landing/blocks/features_list.html',
+        help_text="Product Features Section")),
+        ## Contact Section
+        ('contact_section', blocks.StructBlock([
+            ('in_navigation', blocks.CharBlock(
+                required=False,
+                help_text="Fill if you want this section added in page navigation",)
+            ),
+            ('headline', blocks.CharBlock(required=False)),
+            ('lead', blocks.CharBlock(required=False)),
+            ('content', blocks.RichTextBlock()),
+            ('section_image', ImageChooserBlock(required=False))
+        ], icon='mail', template='landing/blocks/contact_section.html',
+        help_text="Contact Section")),
+        ## Team Section
+        ('team_list', blocks.StructBlock([
+            ('in_navigation', blocks.CharBlock(
+                required=False,
+                help_text="Fill if you want this section added in page navigation",)
+            ),
+            ('headline', blocks.CharBlock()),
+            ('lead', blocks.CharBlock(required=False)),
+            ('content', blocks.RichTextBlock(required=False)),
+            ('team_members', blocks.ListBlock(
+                blocks.StructBlock([
+                    ('name', blocks.CharBlock()),
+                    ('work_title', blocks.CharBlock()),
+                    ('image', ImageChooserBlock(required=False)),
+                    ('description', blocks.RichTextBlock(required=False)),
+                ],
+                icon='user')
+            ,template='landing/blocks/team_member.html')),
+        ], icon='group', template='landing/blocks/team_members.html',
+        help_text="Section for displaying a list of persons")),
+        ## Announcement Section
+        ('announcement_section', blocks.StructBlock([
+            ('in_navigation', blocks.CharBlock(
+                required=False,
+                help_text="Fill if you want this section added in page navigation",)
+            ),
+            ('headline', blocks.CharBlock(required=False)),
+            ('lead', blocks.CharBlock(required=False)),
+            ('content', blocks.RichTextBlock()),
+            ('section_image', ImageChooserBlock(required=False))
+        ], icon='warning', template='landing/blocks/announcement_section.html',
+        help_text="Banner section for special announcements")),
     ], blank=True)
 
     content_panels = Page.content_panels + [
